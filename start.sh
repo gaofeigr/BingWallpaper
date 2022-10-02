a2276c8bc774823e03216224a48a23a1288e2e86
gaofei <gaofeigr@outlook.com>
2022-10-02 23:01:28 +0800

    --

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup java -jar /Users/gaofei/Pictures/BingWallpaper/bin/BingWallpaper.jar  > log.file  2>&1 &
+nohup java -jar -Xms64m -Xmx128m /Users/gaofei/Pictures/BingWallpaper/bin/BingWallpaper.jar  > log.file  2>&1 &
